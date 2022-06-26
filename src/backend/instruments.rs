6fec724481325bf3bb368529d6b7a4f09dd8ae2d
Mike Tsao <mike@sowbug.com>
2022-06-25 19:17:11 -0700

    Sequencer now uses ticks internally

--- src/backend/instruments.rs
+++ src/backend/instruments.rs
@@ -61,7 +61,7 @@ impl DeviceTrait for Oscillator {
             midi::MidiMessageType::NoteOn => {
                 self.frequency = message.to_frequency();
             }
-            midi::MidiMessageType::_NoteOff => {
+            midi::MidiMessageType::NoteOff => {
                 self.frequency = 0.;
             }
         }
@@ -72,28 +72,64 @@ impl DeviceTrait for Oscillator {
     }
 }
 
-pub struct Note {
-    which: u8,
-    when: f32,
+pub struct TimeSignature {
+    numerator: usize,
+    denominator: usize,
+}
+
+impl TimeSignature {
+    pub fn new(numerator: usize, denominator: usize) -> TimeSignature {
+        TimeSignature {
+            numerator,
+            denominator,
+        }
+    }
 }
 pub struct Sequencer {
+    time_signature: TimeSignature,
+    midi_ticks_per_second: usize,
     sinks: Vec<Rc<RefCell<dyn DeviceTrait>>>,
-    note_events: VecDeque<Note>,
+    midi_messages: VecDeque<(usize, MidiMessage)>,
 }
 
 impl Sequencer {
     pub fn new() -> Sequencer {
         Sequencer {
+            time_signature: TimeSignature::new(4, 4),
+            midi_ticks_per_second: 0,
             sinks: Vec::new(),
-            note_events: VecDeque::new(),
+            midi_messages: VecDeque::new(),
         }
     }
 
-    pub fn add_note_on(&mut self, which: u8, when: f32) {
-        self.note_events.push_back(Note { which, when });
+    pub fn set_time_signature(&mut self, numerator: usize, denominator: usize) {
+        self.time_signature = TimeSignature::new(numerator, denominator);
     }
-    pub fn add_note_off(&mut self, which: u8, when: f32) {
-        self.note_events.push_back(Note { which, when });
+
+    pub fn set_midi_ticks_per_second(&mut self, tps: usize) {
+        self.midi_ticks_per_second = tps;
+    }
+
+    pub fn add_message(&mut self, when: usize, message: MidiMessage) {
+        self.midi_messages.push_back((when, message));
+    }
+    pub fn add_note_on(&mut self, when: usize, which: u8) {
+        let midi_message = MidiMessage {
+            status: MidiMessageType::NoteOn,
+            channel: 0,
+            data1: which,
+            data2: 0,
+        };
+        self.midi_messages.push_back((when, midi_message));
+    }
+    pub fn add_note_off(&mut self, when: usize, which: u8) {
+        let midi_message = MidiMessage {
+            status: MidiMessageType::NoteOff,
+            channel: 0,
+            data1: which,
+            data2: 0,
+        };
+        self.midi_messages.push_back((when, midi_message));
     }
 }
 
@@ -103,24 +139,22 @@ impl DeviceTrait for Sequencer {
     }
 
     fn tick(&mut self, clock: &Clock) -> bool {
-        if self.note_events.is_empty() {
+        if self.midi_messages.is_empty() {
             return true;
         }
-        let note = self.note_events.pop_front().unwrap();
-        if clock.real_clock >= note.when {
-            let midi_message = MidiMessage {
-                status: MidiMessageType::NoteOn,
-                channel: 0,
-                data1: note.which,
-                data2: 0,
-            };
+        let (when, midi_message) = self.midi_messages.pop_front().unwrap();
+
+        // TODO(miket): I'm getting a bad feeling about the usize and f32 conversions.
+        let elapsed_midi_ticks: usize =
+            ((clock.real_clock * self.midi_ticks_per_second as f32) as u32) as usize;
+        if elapsed_midi_ticks >= when {
             for i in self.sinks.clone() {
                 i.borrow_mut().handle_midi_message(&midi_message);
             }
         } else {
             // TODO(miket): I had to always pop always and then sometimes re-push because
             // I can't figure out how to get around the borrow checker if I use just a front().
-            self.note_events.push_front(note);
+            self.midi_messages.push_front((when, midi_message));
         }
         false
     }
--- src/backend/midi.rs
+++ src/backend/midi.rs
@@ -5,7 +5,7 @@ use midly::{MidiMessage as MidlyMidiMessage, TrackEventKind};
 
 pub enum MidiMessageType {
     NoteOn = 0x1001,
-    _NoteOff = 0x1000,
+    NoteOff = 0x1000,
 }
 pub struct MidiMessage {
     // status and channel are normally packed into one byte, but for ease of use
@@ -31,40 +31,66 @@ impl MidiReader {
     pub fn load_sequencer(data: &[u8], sequencer: Rc<RefCell<Sequencer>>) {
         let smf = midly::Smf::parse(data).unwrap();
 
-        let mut ticks_per_click: f32 = 0.;
-        let mut seconds_per_tick: f32 = 0.;
+        struct MetaInfo {
+            // Pulses per quarter-note
+            ppq: usize,
+
+            // Microseconds per quarter-note
+            tempo: usize,
+
+            time_signature_numerator: u8,
+            time_signature_denominator_exp: u8,
+        }
+        let mut meta_info = MetaInfo {
+            ppq: match smf.header.timing {
+                midly::Timing::Metrical(ticks_per_beat) => ticks_per_beat.as_int() as usize,
+                _ => 0,
+            },
+            tempo: 0,
+            time_signature_numerator: 0,
+            time_signature_denominator_exp: 0,
+        };
         for track in smf.tracks.iter() {
-            let mut track_time_ticks: u32 = 0; // Each track's relative time references start over at zero
+            let mut track_time_ticks: usize = 0; // The relative time references start over at zero with each track.
             for t in track.iter() {
                 match t.kind {
                     TrackEventKind::Midi {
-                        channel: _,
+                        channel: u4,
                         message,
                     } => {
                         let delta = t.delta;
-                        track_time_ticks += delta.as_int();
+                        track_time_ticks += delta.as_int() as usize;
                         match message {
                             MidlyMidiMessage::NoteOn { key, vel } => {
-                                if vel == 0 {
-                                    sequencer.borrow_mut().add_note_off(
-                                        key.as_int(),
-                                        track_time_ticks as f32 * seconds_per_tick,
-                                    );
-                                    // println!("note {} DE FACTO OFF at time {}", key, time);
+                                let midi_message = if vel == 0 {
+                                    MidiMessage {
+                                        status: MidiMessageType::NoteOff,
+                                        channel: u4.as_int(),
+                                        data1: key.as_int(),
+                                        data2: vel.as_int(),
+                                    }
                                 } else {
-                                    sequencer.borrow_mut().add_note_on(
-                                        key.as_int(),
-                                        track_time_ticks as f32 * seconds_per_tick,
-                                    );
-                                    // println!("note {} ON at time {}", key, time);
-                                }
+                                    MidiMessage {
+                                        status: MidiMessageType::NoteOn,
+                                        channel: u4.as_int(),
+                                        data1: key.as_int(),
+                                        data2: vel.as_int(),
+                                    }
+                                };
+                                sequencer
+                                    .borrow_mut()
+                                    .add_message(track_time_ticks, midi_message);
                             }
-                            MidlyMidiMessage::NoteOff { key, vel: _ } => {
-                                println!("note {} OFF at time {}", key, track_time_ticks);
-                                sequencer.borrow_mut().add_note_off(
-                                    key.as_int(),
-                                    track_time_ticks as f32 * seconds_per_tick,
-                                );
+                            MidlyMidiMessage::NoteOff { key, vel } => {
+                                let midi_message = MidiMessage {
+                                    status: MidiMessageType::NoteOff,
+                                    channel: u4.as_int(),
+                                    data1: key.as_int(),
+                                    data2: vel.as_int(),
+                                };
+                                sequencer
+                                    .borrow_mut()
+                                    .add_message(track_time_ticks, midi_message);
                             }
                             _ => {
                                 // println!("skipping {:?}", message);
@@ -72,21 +98,13 @@ impl MidiReader {
                         }
                     }
                     TrackEventKind::Meta(meta_message) => match meta_message {
-                        midly::MetaMessage::TimeSignature(
-                            _numerator,
-                            _denominator_exp,
-                            cc,
-                            _bb,
-                        ) => {
-                            ticks_per_click = cc as f32;
-                            println!("ticks per click {}", ticks_per_click);
+                        midly::MetaMessage::TimeSignature(numerator, denominator_exp, cc, _bb) => {
+                            meta_info.time_signature_numerator = numerator;
+                            meta_info.time_signature_denominator_exp = denominator_exp;
+                            //meta_info.ppq = cc; WHA???
                         }
                         midly::MetaMessage::Tempo(tempo) => {
-                            println!("microseconds per beat: {}", tempo);
-                            // TODO: handle time signatures
-                            let bpm = 60.0 * 4.0 / (1000000.0 / (tempo.as_int() as f32));
-                            seconds_per_tick = 1. / (ticks_per_click * 4. * 2.);
-                            println!("BPM: {}. seconds per tick: {}", bpm, seconds_per_tick);
+                            meta_info.tempo = tempo.as_int() as usize;
                         }
                         midly::MetaMessage::TrackNumber(track_opt) => {
                             if track_opt.is_none() {
@@ -96,6 +114,33 @@ impl MidiReader {
                             if track_number > 2 {
                                 continue;
                             }
+                        }
+                        midly::MetaMessage::EndOfTrack => {
+                            let time_signature: (usize, usize) = (
+                                meta_info.time_signature_numerator.into(),
+                                2_usize.pow(meta_info.time_signature_denominator_exp.into()),
+                            );
+                            println!(
+                                "Time signature is {}/{}",
+                                time_signature.0, time_signature.1
+                            );
+
+                            let ticks_per_quarter_note: f32 = meta_info.ppq as f32;
+                            let seconds_per_quarter_note: f32 = meta_info.tempo as f32 / 1000000.0;
+                            let ticks_per_second = ticks_per_quarter_note / seconds_per_quarter_note;
+
+                            let bpm: f32 = (60.0 * 1000000.0) / (meta_info.tempo as f32);
+
+                            // https://stackoverflow.com/a/2038364/344467
+                            //                            let ticks_per_second: usize = (bpm * meta_info.tempo) / 60000;
+                            println!("MIDI ticks per second: {}", ticks_per_second);
+                            println!("BPM: {}", bpm);
+                            sequencer
+                                .borrow_mut()
+                                .set_time_signature(time_signature.0, time_signature.1);
+                            sequencer
+                                .borrow_mut()
+                                .set_midi_ticks_per_second(ticks_per_second as usize);
                         }
                         _ => {}
                     },
