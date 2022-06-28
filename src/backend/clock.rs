b7562c3d0f1e551074b39f7f10be338e83a9e64a
Mike Tsao <mike@sowbug.com>
2022-06-28 13:22:07 -0700

    Try adding some unit tests

--- src/backend/clock.rs
+++ src/backend/clock.rs
@@ -17,3 +17,24 @@ impl Clock {
         self.seconds = self.samples as f32 / self.sample_rate as f32;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_clock_mainline() {
+        const SAMPLE_RATE: u32 = 256;
+        let mut clock = Clock::new(SAMPLE_RATE);
+
+        // init state
+        assert_eq!(clock.sample_rate, SAMPLE_RATE);
+        assert_eq!(clock.samples, 0);
+        assert_eq!(clock.seconds, 0.0);
+
+        // after a tick
+        clock.tick();
+        assert_eq!(clock.samples, 1);
+        assert_eq!(clock.seconds, 1.0 / SAMPLE_RATE as f32);
+    }
+}
--- src/backend/effects.rs
+++ src/backend/effects.rs
@@ -56,3 +56,38 @@ impl DeviceTrait for Quietener {
         self.source.borrow().get_audio_sample() * 0.8
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestAlwaysLoudDevice {}
+    impl DeviceTrait for TestAlwaysLoudDevice {
+        fn get_audio_sample(&self) -> f32 {
+            1.
+        }
+    }
+
+    struct TestAlwaysSilentDevice {}
+    impl DeviceTrait for TestAlwaysSilentDevice {
+        fn get_audio_sample(&self) -> f32 {
+            0.
+        }
+    }
+
+    #[test]
+    fn test_mixer_mainline() {
+        let mut mixer = Mixer::new();
+
+        // Nothing
+        assert_eq!(mixer.get_audio_sample(), 0.);
+
+        // One always-loud
+        mixer.add_audio_source(Rc::new(RefCell::new(TestAlwaysLoudDevice {})));
+        assert_eq!(mixer.get_audio_sample(), 1.);
+
+        // One always-loud and one always-quiet
+        mixer.add_audio_source(Rc::new(RefCell::new(TestAlwaysSilentDevice {})));
+        assert_eq!(mixer.get_audio_sample(), 0.5);
+    }
+}
