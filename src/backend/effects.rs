276073c30383f9598254668b7f1e3986adcf4dbe
Mike Tsao <mike@sowbug.com>
2022-06-29 12:34:48 -0700

    Hard-clipping limiter

--- src/backend/effects.rs
+++ src/backend/effects.rs
@@ -57,9 +57,44 @@ impl DeviceTrait for Gain {
     }
 }
 
+pub struct Limiter {
+    source: Rc<RefCell<dyn DeviceTrait>>,
+    min: f32,
+    max: f32,
+}
+impl Limiter {
+    pub fn new(source: Rc<RefCell<dyn DeviceTrait>>, min: f32, max: f32) -> Limiter {
+        Limiter { source, min, max }
+    }
+}
+impl DeviceTrait for Limiter {
+    fn sources_audio(&self) -> bool {
+        true
+    }
+    fn sinks_audio(&self) -> bool {
+        true
+    }
+    fn add_audio_source(&mut self, source: Rc<RefCell<dyn DeviceTrait>>) {
+        self.source = source;
+    }
+    fn get_audio_sample(&self) -> f32 {
+        self.source
+            .borrow()
+            .get_audio_sample()
+            .clamp(self.min, self.max)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    struct TestAlwaysTooLoudDevice {}
+    impl DeviceTrait for TestAlwaysTooLoudDevice {
+        fn get_audio_sample(&self) -> f32 {
+            1.1
+        }
+    }
 
     struct TestAlwaysLoudDevice {}
     impl DeviceTrait for TestAlwaysLoudDevice {
@@ -89,5 +124,22 @@ mod tests {
         // One always-loud and one always-quiet
         mixer.add_audio_source(Rc::new(RefCell::new(TestAlwaysSilentDevice {})));
         assert_eq!(mixer.get_audio_sample(), 0.5);
+    }
+
+    #[test]
+    fn test_gain_mainline() {
+        let loud = Rc::new(RefCell::new(TestAlwaysLoudDevice {}));
+        let gain = Gain::new(loud, 1.1);
+        assert_eq!(gain.get_audio_sample(), 1.1);
+    }
+
+    #[test]
+    fn test_limiter_mainline() {
+        const MAX: f32 = 0.9;
+        let too_loud = Rc::new(RefCell::new(TestAlwaysTooLoudDevice {}));
+        let limiter = Limiter::new(too_loud.clone(), 0.0, MAX);
+
+        assert_eq!(too_loud.borrow().get_audio_sample(), 1.1);
+        assert_eq!(limiter.get_audio_sample(), MAX);
     }
 }
