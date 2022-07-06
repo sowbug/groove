fbf35889f7acf4cedf69f183c3d07b743740f3ff
Mike Tsao <mike@sowbug.com>
2022-07-05 20:06:37 -0700

    Introducing automation

--- src/backend/devices.rs
+++ src/backend/devices.rs
@@ -37,4 +37,9 @@ pub trait DeviceTrait {
     fn add_audio_source(&mut self, _audio_instrument: Rc<RefCell<dyn DeviceTrait>>) {}
     fn connect_midi_sink(&mut self, _audio_instrument: Rc<RefCell<dyn DeviceTrait>>) {}
     fn handle_midi_message(&mut self, _message: &MidiMessage, _clock: &Clock) {}
+//    fn handle_automation(&mut self, _amplitude: f32) {} // TODO: this needs help
+}
+
+pub trait AutomatableTrait {
+    fn handle_automation(&mut self, _value: f32) {}
 }
--- src/primitives/lfos.rs
+++ src/primitives/lfos.rs
@@ -1,6 +1,12 @@
-use crate::backend::devices::DeviceTrait;
+use std::{cell::RefCell, rc::Rc};
 
-struct Lfo {}
+use crate::backend::devices::{DeviceTrait, AutomatableTrait};
+
+struct Lfo {
+    frequency: f32,
+    current_value: f32,
+    targets: Vec<Rc<RefCell<dyn AutomatableTrait>>>,
+}
 
 impl DeviceTrait for Lfo {
     fn sinks_midi(&self) -> bool {
@@ -10,10 +16,26 @@ impl DeviceTrait for Lfo {
     fn sources_automation(&self) -> bool {
         true
     }
+
+    fn tick(&mut self, clock: &crate::backend::clock::Clock) -> bool {
+        let phase_normalized = self.frequency * (clock.seconds as f32);
+        self.current_value = 2.0 * (phase_normalized - (0.5 + phase_normalized).floor());
+        for target in self.targets.iter_mut() {
+            target.borrow_mut().handle_automation(self.current_value);
+        }
+        false
+    }
 }
 
 impl Lfo {
-    pub fn new() -> Lfo {
-        Lfo {}
+    pub fn new(frequency: f32) -> Lfo {
+        Lfo {
+            frequency,
+            current_value: 0.,
+            targets: Vec::new(),
+        }
     }
 }
+
+
+// TODO: is this just extra stuff hung off Oscillator?
