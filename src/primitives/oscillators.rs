13b8f76022f784bc75a8e1ea9b807ac7aae6b85d
Mike Tsao <mike@sowbug.com>
2022-07-09 12:07:12 -0700

    simple noise generator

--- src/primitives/oscillators.rs
+++ src/primitives/oscillators.rs
@@ -12,6 +12,7 @@ pub enum Waveform {
     Square,
     Triangle,
     Sawtooth,
+    Noise,
 }
 
 impl Default for Waveform {
@@ -25,6 +26,9 @@ pub struct Oscillator {
     waveform: Waveform,
     current_sample: f32,
     frequency: f32,
+
+    noise_x1: u32,
+    noise_x2: u32,
 }
 
 // TODO: these oscillators are pure in a logical sense, but they alias badly in the real world
@@ -33,6 +37,8 @@ impl Oscillator {
     pub fn new(waveform: Waveform) -> Oscillator {
         Oscillator {
             waveform,
+            noise_x1: 0x70f4f854,
+            noise_x2: 0xe1e9f0a7,
             ..Default::default()
         }
     }
@@ -52,23 +58,26 @@ impl DeviceTrait for Oscillator {
         true
     }
     fn tick(&mut self, clock: &Clock) -> bool {
-        if self.frequency > 0. {
-            let phase_normalized = self.frequency * (clock.seconds as f32);
-            self.current_sample = match self.waveform {
-                // https://en.wikipedia.org/wiki/Sine_wave
-                Waveform::Sine => (phase_normalized * 2.0 * PI).sin(),
-                // https://en.wikipedia.org/wiki/Square_wave
-                Waveform::Square => (phase_normalized * 2.0 * PI).sin().signum(),
-                // https://en.wikipedia.org/wiki/Triangle_wave
-                Waveform::Triangle => {
-                    4.0 * (phase_normalized - (0.75 + phase_normalized).floor() + 0.25).abs() - 1.0
-                }
-                // https://en.wikipedia.org/wiki/Sawtooth_wave
-                Waveform::Sawtooth => 2.0 * (phase_normalized - (0.5 + phase_normalized).floor()),
+        let phase_normalized = self.frequency * (clock.seconds as f32);
+        self.current_sample = match self.waveform {
+            // https://en.wikipedia.org/wiki/Sine_wave
+            Waveform::Sine => (phase_normalized * 2.0 * PI).sin(),
+            // https://en.wikipedia.org/wiki/Square_wave
+            Waveform::Square => (phase_normalized * 2.0 * PI).sin().signum(),
+            // https://en.wikipedia.org/wiki/Triangle_wave
+            Waveform::Triangle => {
+                4.0 * (phase_normalized - (0.75 + phase_normalized).floor() + 0.25).abs() - 1.0
             }
-        } else {
-            self.current_sample = 0.
-        }
+            // https://en.wikipedia.org/wiki/Sawtooth_wave
+            Waveform::Sawtooth => 2.0 * (phase_normalized - (0.5 + phase_normalized).floor()),
+            // https://www.musicdsp.org/en/latest/Synthesis/216-fast-whitenoise-generator.html
+            Waveform::Noise => {
+                self.noise_x1 ^= self.noise_x2;
+                let tmp = 2.0 * (self.noise_x2 as f32 - (u32::MAX as f32 / 2.0)) / u32::MAX as f32;
+                (self.noise_x2, _) = self.noise_x2.overflowing_add(self.noise_x1);
+                tmp
+            }
+        };
         true
     }
     fn handle_midi_message(&mut self, message: &MidiMessage, _clock: &Clock) {
