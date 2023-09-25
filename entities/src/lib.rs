d49be2450667f83345b38bae7797fbcac591410d
Mike Tsao <mike@sowbug.com>
2023-09-25 10:50:37 -0700

    move entities mod import

--- entities/src/lib.rs
+++ entities/src/lib.rs
@@ -2,32 +2,6 @@
 
 //! The suite of instruments, effects, and controllers supplied with Groove.
 
-/// Controllers implement the [IsController](groove_core::traits::IsController)
-/// trait, which means that they control other devices. An example of a
-/// controller is a [Sequencer](groove_entities::controllers::Sequencer), which
-/// produces MIDI messages.
-///
-/// Generally, controllers produce only control signals, and not audio. But
-/// adapters exist that change one kind of signal into another, such as
-/// [SignalPassthroughController], which is used in
-/// [sidechaining](https://en.wikipedia.org/wiki/Dynamic_range_compression#Side-chaining).
-/// In theory, a similar adapter could be used to change a control signal into
-/// an audio signal.
-pub mod controllers;
-/// Effects implement the [IsEffect](groove_core::traits::IsEffect) trait, which
-/// means that they transform audio. They don't produce their own audio, and
-/// while they don't produce control signals, most of them do respond to
-/// controls. Examples of effects are [Compressor](crate::effects::Compressor),
-/// [BiQuadFilter](crate::effects::filter::BiQuadFilter), and
-/// [Reverb](crate::effects::Reverb).
-pub mod effects;
-/// Instruments play sounds. They implement the
-/// [IsInstrument](groove_core::traits::IsInstrument) trait, which means that
-/// they respond to MIDI and produce [StereoSamples](groove_core::StereoSample).
-/// Examples of instruments are [Sampler](crate::instruments::Sampler) and
-/// [WelshSynth](crate::instruments::WelshSynth).
-pub mod instruments;
-
 #[cfg(test)]
 mod tests {
     use ensnare_core::core::ParameterType;
