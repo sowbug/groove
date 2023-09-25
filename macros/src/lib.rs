6a0a8ebf827f6785f11813239543f2efc5d7b9b7
Mike Tsao <mike@sowbug.com>
2023-09-25 10:07:25 -0700

    remove groove-macros

--- macros/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023 Mike Tsao. All rights reserved.
-
-// PRO TIP: use `cargo expand` to see what's being generated. It requires the
-// nightly toolchain.
-
-// At the moment we have no function-like macros! But we will.
